f35f001cf1d563b953d0b4409f2d8d8b14a7136d
Farukh Safin <webexpert1986@gmail.com>
2020-04-23 11:38:25 +0400

    APR23 11:38

--- telegbot/admin.py
+++ telegbot/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 from .models import Question, Player, Player_Response
 # Register your models here.
 
-admin.site.register(Player)
-admin.site.register(Question)
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'mobil_phone', 'city', 'team')
+
+admin.site.register(Player, PlayerAdmin)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'answer', 'score')
+
+admin.site.register(Question, QuestionAdmin)
+
 admin.site.register(Player_Response)
+
