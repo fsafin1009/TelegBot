1423bdace2ff8d5ef7fa6b884d9da24716350202
Farukh Safin <webexpert1986@gmail.com>
2020-04-24 14:36:51 +0400

    APR24 14:36

--- telegbot/urls.py
+++ telegbot/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.detail, name='detail'),
-
+    path('', views.detail, name = "detail")
 ]
--- telegbot/views.py
+++ telegbot/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 from telegbot.models import Question, Player
 
 
@@ -9,6 +11,11 @@ def index(request):
     context = {'questions_list': questions_list}
     return render(request, 'telegbot/index.html', context)
 
+# class PlayerView(generic.ListView):
+#     model = Player
+#     template_name = 'telegbot/detail.html'
+#     def get_queryset(self):
+#         return Player.objects.all()
 
 def detail(request):
     player_list = Player.objects.all()
